26566c6f36b3a14f981d11effb0a1db9dfe8c846
root <root@gau1.govinddas.com>
2026-02-28 14:29:41 +0000

    first hnshmc commit

---
+++ public/repository/gdrive_upload/version.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Google Drive Upload Repository - version file.
+ *
+ * This plugin replaces the standard "Upload a file" repository tab. Instead of
+ * saving uploaded files on the Moodle server it pushes them directly to
+ * Google Drive via the configured system OAuth account, using the same folder
+ * hierarchy as the core Google Docs repository integration.
+ *
+ * @package    repository_gdrive_upload
+ * @copyright  2026 medi-wiki.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'repository_gdrive_upload';
+$plugin->version   = 2026022101;
+$plugin->requires  = 2024042200; // Moodle 4.4+
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0.0';
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'moodle/moodle',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'moodle/moodle' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
