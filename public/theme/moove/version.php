39cc6995b2002d22e0b51bc2f814aaa0533ea048
Krushal Vataliya <krushalvataliya24@gmail.com>
2026-07-25 19:14:24 +0530

    Feat: Replace entire plugin folders with latest released source code for mod_diary, local_commander, theme_moove, and block_sharing_cart

--- public/theme/moove/version.php
+++ public/theme/moove/version.php
@@ -30,16 +30,17 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_moove';
 
 // This is the version of the plugin.
-$plugin->version = 2026042100;
+$plugin->version = 2026042200;
 
 // This is the named version.
-$plugin->release = '5.2.1';
+$plugin->release = '5.2.2';
 
 // This is a stable release.
 $plugin->maturity = MATURITY_STABLE;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2026041000;
+
 
 // This is a list of plugins, this plugin depends on (and their versions).
 $plugin->dependencies = [
